491b6c96d1937fe11d24cd447d54ff521fb6bfd3
deand925 <97762768+deand925@users.noreply.github.com>
2022-09-27 12:45:14 -0700

    connected to the server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -6,12 +7,11 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello World'
-    });
-  });
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
 
-app.listen(PORT, ()=>{
-    console.log(`Server is sunning on port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
